254fa0d03ef0b3630c403b2838395979aabc1b04
hzqula <ilooqstrasi@gmail.com>
2025-08-09 07:10:05 +0700

    🐔 feat: restructure Landing component to include Hero, Locations, Members, and Programs sections

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,22 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Locations from "@/components/Locations";
+import Members from "@/components/Members";
+import Programs from "@/components/Programs";
 const Landing = () => {
   return (
     <>
-      <Header />
-      <div>Landing</div>
+      <div className="min-h-[100dvh] flex flex-col scroll-smooth ">
+        <Header />
+        <main className="flex-1">
+          <Hero />
+          <Locations />
+          {/* <About /> */}
+          <Members />
+          {/* <Gallery /> */}
+          <Programs />
+        </main>
+      </div>
     </>
   );
 };
