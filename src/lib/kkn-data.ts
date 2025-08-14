22eafa00fe417661be1ad15d9c147c0182804928
hzqula <ilooqstrasi@gmail.com>
2025-08-14 17:23:02 +0700

    🐔 feat: update program categories to include 'Agama' for better classification

--- src/lib/kkn-data.ts
+++ src/lib/kkn-data.ts
@@ -241,7 +241,7 @@ export const programs: Program[] = [
     ownerIds: ["m9"],
     date: "Agustus 2025",
     status: "Selesai",
-    categories: ["Psikologi", "Teknologi"],
+    categories: ["Agama", "Teknologi"],
   },
 ];
 
